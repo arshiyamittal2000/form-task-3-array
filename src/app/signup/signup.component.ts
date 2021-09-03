f5940ccbb914a677140933c97fee1ebb59c658fa
arshiyamittal2000 <arshiya.mittal2000@gmail.com>
2021-09-03 12:27:54 +0530

    commit on master

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { SignupModel } from './signupModel';
+
+
+@Component({
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
+})
+export class SignupComponent {
+
+  genders = ['Male', 'Female'];
+  model = new SignupModel();
+  submitted = false;
+  confirm = false;
+
+  onSubmit() { this.submitted = true; }
+
+  confirmSubmit() {
+    this.confirm = true; 
+    this.submitted = false;
+
+    console.log('Its confirmed!');
+  }
+}
---
+++ src/app/signup/signupModel.ts
@@ -0,0 +1,11 @@
+export class SignupModel {
+
+    constructor(
+     
+      public fname: string,
+      public lname: string,
+      public gender: string,
+      public mname?: string
+    ) { }
+  
+  }
